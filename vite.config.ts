3de58da597abb70068952eca3b7eec0688c09100
simo <wambuisimon712@gmail.com>
2023-09-29 12:20:46 +0300

    lets see

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-declare module 'vite' {
-  interface BuildOptions {
-    base : string;
-  }
-}
+// declare module 'vite' {
+//   interface BuildOptions {
+//     base : string;
+//   }
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    base: '/',
-  }
+  base: '/vite-deploy/'
+  // build: {
+    // base: '/vite-react-router/',
+  // }
 })
