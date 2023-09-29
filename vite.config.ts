8953dc03a60f59bf311f0b1fbecc08d3c09b591a
simo <wambuisimon712@gmail.com>
2023-09-29 11:41:01 +0300

    push

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+declare module 'vite' {
+  interface BuildOptions {
+    base : string;
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    base: '/',
+  }
 })
